a123ba7b7fcdddb4f9a9f670e7f46a0bc3ac08d3
shuowen5656 <2260755767@qq.com>
2021-11-07 19:48:17 +0800

    [新增代码]新增算法主项目、认真写算法、像新公司进发！

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Map<String, String> map = new  HashMap<>();
+        map.put("1", "hehe");
+
+        System.out.println(map.toString());
+    }
+}
