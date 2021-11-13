d80b3275cfb1d27f53a43c6c47c627f98b16b3a8
shuowen5656 <2260755767@qq.com>
2021-11-13 12:51:30 +0800

    [新增代码]两个栈组成的队列

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Integer> integers = Arrays.asList(2, 1, 5, 3, 9, 0);
+
+        try{
+            List<Integer> integers = Arrays.asList(2, 1, 5, 3, 9, 0);
 
 
-        /**
-         * MyStack1测试用例
-         */
+            /**
+             * MyStack1测试用例
+             */
 //        MyStack1 myStack1 = new MyStack1();
 //        myStack1.push(2);
 //        myStack1.push(1);
@@ -23,15 +25,19 @@ public class Main {
 
 //        Class clazz = MyStack1.class;
 
-        /**
-         * MyStack2 测试用例
-         */
-        MyStack2 myStack2 = new MyStack2();
-        for (Integer integer : integers){
-            myStack2.push(integer);
+            /**
+             * MyStack2 测试用例
+             */
+            MyStack2 myStack2 = new MyStack2();
+            for (Integer integer : integers){
+                myStack2.push(integer);
+            }
+            System.out.println(myStack2.toString());
+            System.out.println(myStack2.getMin());
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        System.out.println(myStack2.toString());
-        System.out.println(myStack2.getMin());
+
 
 
     }
