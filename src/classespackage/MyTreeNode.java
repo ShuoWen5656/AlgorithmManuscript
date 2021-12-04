79c3542036b6dae6240f928d78fdc471133ce838
ShuoWen5656 <2260755767@qq.com>
2021-12-04 21:57:28 +0800

    [新增代码]最大值树，栈获取左边或右边第一个比自己大的值

---
+++ src/classespackage/MyTreeNode.java
@@ -0,0 +1,51 @@
+package classespackage;
+
+/**
+ * @author swzhao
+ * @date 2021/12/2 9:52 下午
+ * @Discreption <>树节点
+ */
+public class MyTreeNode {
+
+    /**
+     * 左节点
+     */
+    public MyTreeNode left;
+    /**
+     * 右节点
+     */
+    public MyTreeNode right;
+    /**
+     * 节点值
+     */
+    public Integer data;
+
+
+    public MyTreeNode(Integer data) {
+        this.data = data;
+    }
+
+    public MyTreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(MyTreeNode left) {
+        this.left = left;
+    }
+
+    public MyTreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(MyTreeNode right) {
+        this.right = right;
+    }
+
+    public Integer getData() {
+        return data;
+    }
+
+    public void setData(Integer data) {
+        this.data = data;
+    }
+}
