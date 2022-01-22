2bb6d9a285cec00d1d7293fd7221e1e5aa5d4941
ShuoWen5656 <2260755767@qq.com>
2022-01-22 13:06:57 +0800

    [新增代码]1、将单向链表按照某值划分成左边小，中间相等，右边大于的形式 2、判断回文单链表

---
+++ src/classespackage/IsPalindRome.java
@@ -0,0 +1,36 @@
+package classespackage;
+
+import dataConstruct.LinkNode;
+
+import java.util.Stack;
+
+/**
+ * @author swzhao
+ * @date 2022/1/16 8:07 下午
+ * @Discreption <> 是否回文链表
+ */
+public class IsPalindRome {
+    public static boolean isPalindRome(LinkNode head){
+        try{
+            LinkNode cur = head;
+            LinkNode cur2 = head;
+            Stack<LinkNode> linkNodes = new Stack<>();
+            while (cur != null){
+                linkNodes.push(cur);
+                cur = cur.getNext();
+            }
+            // 比对
+            while (cur2 != null){
+                LinkNode stackNode = linkNodes.pop();
+                if(cur2.getValue() != stackNode.getValue()){
+                    return false;
+                }
+                cur2 = cur2.getNext();
+            }
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
---
+++ src/classespackage/listPartition.java
@@ -0,0 +1,9 @@
+package classespackage;
+
+/**
+ * @author swzhao
+ * @date 2022/1/22 1:03 下午
+ * @Discreption <>将单链表按照某个值分为小于的（左），等于的（中），大于的
+ */
+public class listPartition {
+}
