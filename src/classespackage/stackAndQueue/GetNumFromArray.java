ee25930b3b92d21326a2612f3d89eb38441bf152
ShuoWen5656 <2260755767@qq.com>
2022-08-22 22:22:33 +0800

    [新增代码][二轮测试并发表]1、 最大值减去最小值小于等于num的子数组数量

--- src/classespackage/stackAndQueue/GetNumFromArray.java
+++ src/classespackage/stackAndQueue/GetNumFromArray.java
@@ -1,24 +1,28 @@
 package classespackage.stackAndQueue;
 
+import sun.java2d.pipe.AATextRenderer;
+
+import java.util.Deque;
 import java.util.LinkedList;
 
 /**
  * @author swzhao
  * @date 2021/12/15 10:20 下午
- * @Discreption <>数组array，获取满足最大值-最小值<num的子数组数量
+ * @Discreption <>最大值减去最小值小于等于num的子数组数量
+ *     数组array，获取满足最大值-最小值<num的子数组数量
  */
 public class GetNumFromArray {
 
     LinkedList<Integer> qmax;
     LinkedList<Integer> qmin;
 
-    public GetNumFromArray(LinkedList<Integer> qmax, LinkedList<Integer> qmin) {
+    public GetNumFromArray() {
         this.qmax = new LinkedList<>();
         this.qmin = new LinkedList<>();
     }
 
 
-    public Integer getMinNum(Integer[] array, int num){
+    public Integer getMinNum(int[] array, int num){
         try{
             // 头
             int i = 0;
@@ -69,5 +73,47 @@ public class GetNumFromArray {
     }
 
 
+    public static int getNumByArr(int[] arr, int num){
+        Deque<Integer> maxq = new LinkedList<>();
+        Deque<Integer> minq = new LinkedList<>();
+        int res = 0;
+        if (arr == null || arr.length == 0){
+            return 0;
+        }
+        int j = 0;
+        for (int i = 0;i < arr.length;i++){
+            for (; j < arr.length; j++){
+                while (!maxq.isEmpty() && arr[j] > arr[maxq.getLast()]){
+                    maxq.pollLast();
+                }
+                while (!minq.isEmpty() && arr[j] < arr[minq.getLast()]){
+                    minq.pollLast();
+                }
+                maxq.offer(j);
+                minq.offer(j);
+
+                if (arr[maxq.getFirst()] - arr[minq.getFirst()] > num){
+                    break;
+                }
+            }
+            if (arr[i] == arr[maxq.getFirst()]){
+                maxq.pollFirst();
+            }
+            if (arr[i] == arr[minq.getFirst()]){
+                minq.pollFirst();
+            }
+            res+=j-i;
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int[] ints = {3, 5, 4, 1, 2};
+        int numByArr = getNumByArr(ints, 3);
+        //GetNumFromArray getNumFromArray = new GetNumFromArray();
+        //Integer numByArr = getNumFromArray.getMinNum(ints, 3);
+        System.out.println(numByArr);
+    }
 
 }
