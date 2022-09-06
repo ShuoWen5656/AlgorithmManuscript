16d1544c586b40fe94e8fc87428f6b07f95ffc19
ShuoWen5656 <2260755767@qq.com>
2022-09-06 19:49:26 +0800

    [新增代码][二轮测试并发表]1、删除重复出现的节点

--- src/classespackage/link/RemoveRep.java
+++ src/classespackage/link/RemoveRep.java
@@ -1,5 +1,6 @@
 package classespackage.link;
 
+import classespackage.CommonUtils;
 import dataConstruct.LinkNode;
 
 import java.util.HashSet;
@@ -45,4 +46,40 @@ public class RemoveRep {
             return head;
         }
     }
+
+
+
+    public static LinkNode remove2(LinkNode head){
+        if (head == null || head.getNext() == null){
+            return head;
+        }
+        LinkNode cur = head;
+        while (cur != null){
+            // 以当前节点为基准开始
+            LinkNode next = cur.getNext();
+            LinkNode pre = cur;
+            while (next != null){
+                if (next.getValue().equals(cur.getValue())){
+                    pre.setNext(next.getNext());
+                    next = pre.getNext();
+                }else {
+                    next = next.getNext();
+                    pre = pre.getNext();
+                }
+            }
+            cur = cur.getNext();
+        }
+        return head;
+    }
+
+
+    public static void main(String[] args) {
+        LinkNode head = CommonUtils.getLinkNodeListByArr(new int[]{1, 1,2, 2, 3, 4, 4, 5, 5});
+        LinkNode linkNode = remove(head);
+        CommonUtils.printLinkNode(linkNode);
+    }
+
+
+
+
 }
