9c04c1d2908a3cbedf7e6d4d343b983a9e611d0c
ShuoWen5656 <2260755767@qq.com>
2022-10-09 19:40:19 +0800

    [新增代码][二轮测试并发表]1、获取二叉树的最长节点间距离

--- src/classespackage/tree/MaxDistance.java
+++ src/classespackage/tree/MaxDistance.java
@@ -1,5 +1,6 @@
 package classespackage.tree;
 
+import classespackage.CommonUtils;
 import dataConstruct.MyTreeNode;
 
 /**
@@ -45,6 +46,49 @@ public class MaxDistance {
         int curMax = maxFromLeft + 1 + maxFromRight;
         record[0] = Math.max(maxFromLeft, maxFromRight) + 1;
         return Math.max(Math.max(lmax, rmax), curMax);
+    }
+
+
+    /**
+     * 主方法
+     * @param root
+     * @return
+     */
+    public static int getMaxPath(MyTreeNode root){
+        if (root == null){
+            return 0;
+        }
+        return myProcess(root, 0)[0];
+    }
+
+    /**
+     *
+     * @param root
+     * @param level
+     * @return int[0] 当前树的最大距离，int[1] 当前树的最大深度
+     */
+    private static int[] myProcess(MyTreeNode root, int level) {
+        if (root == null){
+            // 最大距离为0， 当前深度为0
+            return new int[]{-1, 0};
+        }
+        // 先获取左右子树的最大距离和深度
+        int[] leftR = myProcess(root.getLeft(), level + 1);
+        int leftH = leftR[1];
+        int[] rightR = myProcess(root.getRight(), level + 1);
+        int rightH = rightR[1];
+        // 开始计算当前节点的最大距离
+        int curMax = leftH + rightH + 1;
+        // 计算当前层深度
+        int curH = Math.max(leftH, rightH) + 1;
+        return new int[]{curMax, curH};
+    }
+
+
+    public static void main(String[] args) {
+        MyTreeNode root = CommonUtils.getCompleteBinaryTree(new int[]{1, 2, 3, 4, 5, 6, 7});
+        int maxPath = getMaxPath(root);
+        System.out.println(maxPath);
     }
 
 }
