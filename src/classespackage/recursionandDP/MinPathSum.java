36f11de3c4b4f387457e6a1935ce4965f935301e
ShuoWen5656 <2260755767@qq.com>
2022-10-20 22:08:33 +0800

    [新增代码][二轮测试并发表]1、矩阵的最小路径和

--- src/classespackage/recursionandDP/MinPathSum.java
+++ src/classespackage/recursionandDP/MinPathSum.java
@@ -1,5 +1,7 @@
 package classespackage.recursionandDP;
 
+import classespackage.stackAndQueue.catDogQueue.Pet;
+
 /**
  * @author swzhao
  * @date 2022/4/18 9:29 上午
@@ -79,5 +81,102 @@ public class MinPathSum {
         }
         return array[array.length - 1];
     }
+
+
+    /**
+     * 递归方法
+     * @param arr
+     * @return
+     */
+    public static int minPath(int[][] arr) {
+        if (arr == null || arr.length == 0 || arr[0].length == 0) {
+             return 0;
+        }
+        return process(arr, arr.length - 1, arr[0].length - 1);
+    }
+
+    /**
+     * 递归主体
+     * 返回到达arr[i][j] 的最小距离
+     * @param arr
+     * @param i 横坐标
+     * @param j 纵坐标
+     */
+    private static int process(int[][] arr, int i, int j) {
+        if (i == 0 && j == 0) {
+            return arr[i][j];
+        }else if (i == 0) {
+            return arr[i][j] + process(arr, i, j-1);
+        }else if (j == 0) {
+            return arr[i][j] + process(arr, i-1 , j);
+        }else {
+            // 找到前面或者上面较小的那个
+            return arr[i][j] + Math.min(process(arr, i-1, j), process(arr, i, j-1));
+        }
+    }
+
+
+    /**
+     * 动态规划最小路径
+     * @param arr
+     * @return
+     */
+    public static int minPathForDP(int[][] arr) {
+        if (arr == null || arr.length == 0 || arr[0].length == 0) {
+            return 0;
+        }
+        // 动态规划矩阵dp[i][j]代表到arr[i][j]的最短路径和
+        int[][] dp = new int[arr.length][arr[0].length];
+        dp[0][0] = arr[0][0];
+        for (int i = 1; i < arr.length; i++) {
+            dp[0][i] = dp[0][i-1] + arr[0][i];
+        }
+        for (int j = 1; j < arr[0].length; j++) {
+            dp[j][0] = dp[j-1][0] + arr[j][0];
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j < arr[0].length; j++) {
+                dp[i][j] = arr[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[arr.length-1][arr.length-1];
+    }
+
+    /**
+     * 动态规划最小路径
+     * 空间降低一个维度
+     * @param arr
+     * @return
+     */
+    public static int minPathForDPReduceMem(int[][] arr) {
+        if (arr == null || arr.length == 0 || arr[0].length == 0) {
+            return 0;
+        }
+        int[] dp = new int[arr.length];
+        dp[0] = arr[0][0];
+        for (int i = 1; i < arr.length; i++) {
+            dp[i] = dp[i-1] + arr[0][i];
+        }
+        for (int i = 1; i < arr.length; i++) {
+            dp[0] += arr[i][0];
+            for (int j = 1; j < arr.length; j++) {
+                dp[j] = arr[i][j] + Math.min(dp[j-1], dp[j]);
+            }
+        }
+        return dp[arr.length-1];
+    }
+
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 3, 5, 9},
+                {8, 1, 3, 4},
+                {5, 0, 6, 1},
+                {8, 8, 4, 0}
+        };
+        System.out.println(minPathForDPReduceMem(arr));
+    }
+
+
 
 }
