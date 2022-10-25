e43747f4b5c9d7589c80c05eb63a28be8ad25c09
ShuoWen5656 <2260755767@qq.com>
2022-10-25 22:39:16 +0800

    [新增代码][二轮测试并发表]1、最长递增子序列测试

--- src/classespackage/recursionandDP/MaxASCSubSeq.java
+++ src/classespackage/recursionandDP/MaxASCSubSeq.java
@@ -1,5 +1,7 @@
 package classespackage.recursionandDP;
 
+import classespackage.CommonUtils;
+
 import java.util.Arrays;
 
 /**
@@ -100,5 +102,119 @@ public class MaxASCSubSeq {
     }
 
 
+    /**
+     * 获取数组中最长递增序列长度的dp数组
+     * @return
+     */
+    public static int[] getMaxLenFromArrCP(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+        // dp[i] 代表 递增子序列以arr[i]结尾时的最长长度
+        int[] dp = new int[arr.length];
+        dp[0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            int maxLen = Integer.MIN_VALUE;
+            for (int j = i; j >= 0; j--) {
+                if (arr[j] < arr[i]) {
+                    maxLen = Math.max(maxLen, dp[j]);
+                }
+            }
+            dp[i] = maxLen == Integer.MIN_VALUE? 1 : maxLen + 1;
+        }
+        return dp;
+    }
 
+    /**
+     * 获取最长子序列
+     * 时间复杂度O(N^2)
+     * @param arr
+     * @return
+     */
+    public static int[] getSeqFromArrCP(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+        int[] dp = getMaxLenFromArrCP(arr);
+        int max = Integer.MIN_VALUE;
+        int maxIndex = -1;
+        for (int i = 0; i < dp.length; i++){
+            if (dp[i] > max) {
+                max = dp[i];
+                maxIndex = i;
+            }
+        }
+        int index = 0;
+        int[] res1 = new int[arr.length];
+        res1[index++] = arr[maxIndex];
+        for (int i = maxIndex-1; i >= 0; i--) {
+            if (arr[i] < arr[maxIndex] && dp[i] == dp[maxIndex] - 1){
+                res1[index++] = arr[i];
+                maxIndex = i;
+            }
+        }
+        int[] res = new int[index];
+        // 翻转数组
+        while (index > 0) {
+            res[res.length-index] = res1[index-1];
+            index--;
+        }
+        return res;
+    }
+    /**
+     * 获取数组中最长递增序列长度的dp数组
+     * @return
+     */
+    public static int[] getMaxLenFromArrCPPlus(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+        int[] dp = new int[arr.length];
+        dp[0] = 1;
+        int[] ends = new int[arr.length];
+        int right = 0;
+        // 代表到arr[0]为止，长度为right+1的所有子序列中，结尾最小的数为arr[0]
+        ends[right] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            // 找ends最左边的大于等于arr[i]的数
+            int index = findIndexFromEnds(ends, arr[i], right);
+            if (index == -1) {
+                // 没有找到，说明ends的有效区域需要增加
+                ends[++right] = arr[i];
+                dp[i] = right+1;
+            }else {
+                // 找到了，则arr[i]结尾的最长子数组长度也找到了，并且需要更新最小结尾
+                dp[i] = index+1;
+                ends[index] = arr[i];
+            }
+        }
+        return dp;
+    }
+
+    /**
+     * 找到ends左边第一个比value大的数
+     * @param ends
+     * @param value
+     * @return
+     */
+    private static int findIndexFromEnds(int[] ends, int value, int right) {
+        int left = 0;
+        int r = right;
+        while (left <= r) {
+            int mid = (left+r)/2;
+            if (ends[mid] > value) {
+                r = mid-1;
+            }else if (ends[mid] < value) {
+                left = mid+1;
+            }else {
+                // 相等
+                return mid;
+            }
+        }
+        return left > right? -1 : left;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMaxLenFromArrCPPlus(new int[]{2,1,5,3,6,4,8,9,7}));
+    }
 }
