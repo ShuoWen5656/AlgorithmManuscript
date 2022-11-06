299db801c9a825c6a3021f07e08f42359fb0ae4f
ShuoWen5656 <2260755767@qq.com>
2022-11-06 11:07:25 +0800

    [新增代码][二轮测试并发表]1、龙与地下城游戏

--- src/classespackage/recursionandDP/MinHP.java
+++ src/classespackage/recursionandDP/MinHP.java
@@ -78,4 +78,68 @@ public class MinHP {
         }
     }
 
+
+    /**
+     * 二轮：经典动态规划
+     * @param map
+     * @return
+     */
+    public static int minHpCP(int[][] map) {
+        if (map == null || map.length == 0 || map[0].length == 0) {
+            return 0;
+        }
+        int row = map.length;
+        int col = map[0].length;
+        int[][] dp = new int[row][col];
+        dp[row-1][col-1] = 1 - map[row-1][col-1] > 0 ? 1 - map[row-1][col-1] : 1;
+        for (int i = row-2; i >= 0; i--) {
+            dp[i][col-1] = dp[i+1][col-1] - map[i][col-1] > 0 ? dp[i+1][col-1] - map[i][col-1] : 1;
+        }
+        for (int j = col-2; j >= 0; j--) {
+            dp[row-1][j] = dp[row-1][j+1] - map[row-1][j] > 0 ? dp[row-1][j+1] - map[row-1][j] : 1;
+        }
+        for (int i = row-2; i >= 0; i--) {
+            for (int j = col-2; j >= 0; j--) {
+                dp[i][j] = Math.min(dp[i+1][j], dp[i][j+1]) - map[i][j] > 0 ? Math.min(dp[i+1][j], dp[i][j+1]) - map[i][j] : 1;
+            }
+        }
+        return dp[0][0];
+    }
+
+    /**
+     * 二轮：动态规划降低空间复杂度版本
+     * @param map
+     * @return
+     */
+    public static int minHPCPReduceMem(int[][] map) {
+        if (map == null || map.length == 0 || map[0].length == 0) {
+            return 0;
+        }
+        int row = map.length;
+        int col = map[0].length;
+        int[] dp = new int[col];
+        dp[col-1] = 1 - map[row-1][col-1] > 0 ? 1 - map[row-1][col-1] : 1;
+        for (int j = col-2; j >= 0; j --) {
+            dp[j] = dp[j+1] - map[row-1][j] > 0 ? dp[j+1] - map[row-1][j] : 1;
+        }
+        for (int i = row-2; i >= 0; i--) {
+            // 每一行先初始化第一个
+            dp[col-1] = dp[col-1] - map[i][col-1] > 0 ? dp[col-1] - map[i][col-1] : 1;
+            for (int j = col-2; j >= 0 ; j--) {
+                dp[j] = Math.min(dp[j], dp[j+1]) - map[i][j] > 0 ? Math.min(dp[j], dp[j+1]) - map[i][j] :1;
+            }
+        }
+        return dp[0];
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(minHPCPReduceMem(new int[][]{
+                {-2, -3, 3},
+                {-5, -10, 1},
+                {0, 30, -5}
+        }));
+    }
+
+
 }
