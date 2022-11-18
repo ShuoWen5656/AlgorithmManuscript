c68ceeb74d4edbee4d7b7f84bb93843506b2020f
ShuoWen5656 <2260755767@qq.com>
2022-11-18 20:38:59 +0800

    [新增代码][二轮测试并发表]1、数组中的最长连续序列

--- src/classespackage/recursionandDP/LongestConsecutive.java
+++ src/classespackage/recursionandDP/LongestConsecutive.java
@@ -1,5 +1,7 @@
 package classespackage.recursionandDP;
 
+import dataConstruct.DisJointSetCP;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,6 +12,11 @@ import java.util.Map;
  */
 public class LongestConsecutive {
 
+    /**
+     * 该解法有误，没有将边界值更新
+     * @param array
+     * @return
+     */
     public static int longestArrayLen(int[] array){
         try {
             if(array == null || array.length == 0){
@@ -49,5 +56,58 @@ public class LongestConsecutive {
             return 0;
         }
     }
+
+
+    /**
+     * 二轮测试：map法
+     * @param arr
+     * @return
+     */
+    public static int maxSeqLenCp1(int[] arr) {
+        if(arr == null || arr.length == 0){
+            return 0;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int max = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                // 已经出现过的就不计算了
+                continue;
+            }
+            // 先将当前值放入
+            map.put(arr[i], 1);
+            // 检查前面有没有连续的
+            if (map.containsKey(arr[i] - 1)) {
+                max = Math.max(max, merge(map, arr[i] - 1, arr[i]));
+            }
+            // 检查后面有没有连续的
+            if (map.containsKey(arr[i] + 1)) {
+                max = Math.max(max, merge(map, arr[i], arr[i] + 1));
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 合并map，只更新边界值为最大值，因为中间值不会拿出来再判断了
+     * @param map
+     * @param pre
+     * @param next
+     * @return
+     */
+    private static int merge(HashMap<Integer, Integer> map, int pre, int next) {
+        int left = pre - map.get(pre) + 1;
+        int right = map.get(next) + next - 1;
+        int len = right - left + 1;
+        // 更新边界值
+        map.put(left, len);
+        map.put(right, len);
+        return len;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSeqLenCp1(new int[]{100, 4, 200, 1,3,2}));
+    }
+
 
 }
