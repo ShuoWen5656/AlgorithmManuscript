f547650d02cf33d3792a350c6e1564b669df862f
ShuoWen5656 <2260755767@qq.com>
2022-11-20 09:44:28 +0800

    [新增代码][二轮测试并发表]1、判断字符串是否互为变形词

--- src/classespackage/stringproblem/IsDeformation.java
+++ src/classespackage/stringproblem/IsDeformation.java
@@ -44,4 +44,29 @@ public class IsDeformation {
         }
     }
 
+
+    /**
+     * 是否互为变形词
+     * @param str1
+     * @param str2
+     * @return
+     */
+    public static boolean isDeformationCp1(String str1, String str2) {
+        if (str1 == null || str2 == null ||  str1.length() != str2.length()) {
+            return false;
+        }
+        int[] map = new int[255];
+        char[] chars1 = str1.toCharArray();
+        char[] chars2 = str2.toCharArray();
+        for (int i = 0;  i < chars1.length; i++) {
+            map[chars1[i]]++;
+        }
+        for (int i = 0; i < chars2.length; i++) {
+            map[chars2[i]]--;
+            if (map[chars2[i]] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
