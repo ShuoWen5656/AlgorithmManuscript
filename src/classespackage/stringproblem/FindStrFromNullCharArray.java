8687dfa7af945fa51a19a81c3bbaf9836b9cbe8a
ShuoWen5656 <2260755767@qq.com>
2022-12-07 21:49:03 +0800

    [新增代码][二轮测试并发表]1、查找非null的字符串

--- src/classespackage/stringproblem/FindStrFromNullCharArray.java
+++ src/classespackage/stringproblem/FindStrFromNullCharArray.java
@@ -68,4 +68,95 @@ public class FindStrFromNullCharArray {
     }
 
 
+    /**
+     * 二轮测试： 查找字符串
+     * @param strs
+     */
+    public static Integer findCp1(String[] strs, String target) {
+        if (strs == null || target == null || strs.length == 0){
+            return -1;
+        }
+        int left = 0;
+        int right = strs.length-1;
+        while (strs[right] == null){
+            right--;
+        }
+        while (strs[left] == null) {
+            left++;
+        }
+        while (left < right) {
+            int mid = (left + right) /2+1;
+
+            if (strs[mid] == null) {
+                // null值的两个边界
+                int leftNotNull = mid;
+                int rightNotNull = mid;
+                int tem = mid + 1;
+                while (strs[tem] == null) {
+                    tem++;
+                }
+                rightNotNull = tem;
+                tem = mid;
+                while (strs[tem] == null) {
+                    tem--;
+                }
+                leftNotNull = tem;
+                // 处理相等情况
+                if (target.equals(strs[rightNotNull])) {
+                    left = rightNotNull;
+                    break;
+                }
+                if (target.equals(strs[leftNotNull])) {
+                    left = leftNotNull;
+                    break;
+                }
+                // 处理不存在情况
+                if (target.compareTo(strs[rightNotNull]) < 0 && target.compareTo(strs[leftNotNull]) > 0) {
+                    return -1;
+                } else if (target.compareTo(strs[rightNotNull]) > 0) {
+                    left = rightNotNull;
+                }else if (target.compareTo(strs[leftNotNull]) < 0){
+                    right = leftNotNull;
+                }
+            }else {
+                // 处理相等情况
+                if (target.equals(strs[mid])) {
+                    left = mid;
+                    break;
+                }else if (target.compareTo(strs[mid]) < 0) {
+                    String cur = strs[mid];
+                    while (mid >= left && (strs[mid] == null || cur.equals(strs[mid]))) {
+                        mid--;
+                    }
+                    right = mid;
+                }else {
+                    String cur = strs[mid];
+                    while (mid <= right && (strs[mid] == null || cur.equals(strs[mid]))) {
+                        mid++;
+                    }
+                    left = mid;
+                }
+            }
+        }
+        if (target.equals(strs[left])) {
+            int i = left;
+            while (i >= 0 && (target.equals(strs[i]) || strs[i] == null)) {
+                if (target.equals(strs[i])) {
+                    left = i;
+                }
+                i--;
+            }
+            return left;
+        }else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findCp1(new String[]{null, null, "a",null, null, "b", null, "c"}, "a"));
+
+
+    }
+
+
 }
