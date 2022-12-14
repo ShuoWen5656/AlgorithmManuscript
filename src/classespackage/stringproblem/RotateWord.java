9c96a9dfa12ceea95025f7a749a102fc5bde1113
ShuoWen5656 <2260755767@qq.com>
2022-12-14 22:13:12 +0800

    [新增代码][二轮测试并发表]1、字符串翻转

--- src/classespackage/stringproblem/RotateWord.java
+++ src/classespackage/stringproblem/RotateWord.java
@@ -36,6 +36,34 @@ public class RotateWord {
 
 
     /**
+     * 二轮测试：翻转字符串,内部不翻转
+     */
+    public static void reverseCP1(char[] chars) {
+        if (chars == null || chars.length == 0) {
+            return;
+        }
+        // 先翻转
+        reverseChar(chars, 0, chars.length-1);
+        int left = -1, right = -1;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != ' ') {
+                // 可能是开头，也可能是中间
+                left = i == 0 || chars[i-1] == ' ' ? i : left;
+                // 可能是中间，也可能是结尾
+                right = i == chars.length-1 || chars[i+1] == ' ' ? i : -1;
+            }
+            if (left != -1 && right != -1) {
+                // 左右都找到了，直接翻转
+                reverseChar(chars, left, right);
+                // 重置
+                left = -1;
+                right = -1;
+            }
+        }
+    }
+
+
+    /**
      * 翻转char字符串的一部分
      * @param chars
      * @return
@@ -57,7 +85,7 @@ public class RotateWord {
 
 
     /**
-     * 将chars左边size个元素和右边调换位置
+     *  将chars左边size个元素和右边调换位置
      * 全部逆序后各自逆序即可
      * @param chars
      * @return
@@ -75,6 +103,18 @@ public class RotateWord {
             e.printStackTrace();
             return null;
         }
+    }
+
+    /**
+     * 问题二： 二轮测试，方法一
+     */
+    public static void reverseBySizeCp2(char[] chars, int size) {
+        if (chars == null || chars.length == 0) {
+            return;
+        }
+        reverseChar(chars, 0, size - 1);
+        reverseChar(chars, size, chars.length-1);
+        reverseChar(chars, 0, chars.length-1);
     }
 
     /**
@@ -127,6 +167,67 @@ public class RotateWord {
     }
 
 
+    /**
+     * 二轮测试：递归法
+     * @param chars
+     * @param size
+     */
+    public static void reverseBySize2Cp2(char[] chars, int size) {
+        if (chars == null || chars.length == 0 || size <= 0) {
+            return;
+        }
+        reverseBySize2CpHelper(chars, 0, size-1, size, chars.length-1);
+    }
+
+    /**
+     * 递归主体
+     * @param chars
+     * @param left1
+     * @param right1
+     * @param left2
+     * @param right2
+     */
+    private static void reverseBySize2CpHelper(char[] chars, int left1, int right1, int left2, int right2) {
+        // 先做一个长度差值
+        int len1 = right1 - left1 + 1;
+        int len2 = right2 - left2 + 1;
+        // 将值复制一份，最好不要使用参数
+        int left1New = left1;
+        int right1New = right1;
+        int left2New = left2;
+        int right2New = right2;
+        int offset = Math.abs(len1 - len2);
+
+        // 注意这里只有右边长的时候才需要offset
+        if (len2 > len1) {
+            left2New += offset;
+        }
+        // 开始替换:这里其实一个判断就可以
+        while (left1New <= right1New && left2New <= right2New) {
+            char tmp = chars[left1New];
+            chars[left1New] = chars[left2New];
+            chars[left2New] = tmp;
+            left1New++;
+            left2New++;
+        }
+        // 替换完成后,开始准备下一轮递归
+        if (len1 > len2) {
+            reverseBySize2CpHelper(chars, left1New, right1, left2, right2);
+        }else if (len1 == len2){
+            return;
+        }else {
+            reverseBySize2CpHelper(chars, left1, right1, left2, left2 + offset - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] chars = "pig loves dog".toCharArray();
+
+        reverseCP1(chars);
+    }
+
+
+
     public static void exchange(char[] chars, int start, int end, int size){
         int i = end - size + 1;
         int index = 0;
@@ -136,6 +237,10 @@ public class RotateWord {
             chars[index++] = tem;
         }
     }
+
+
+
+
 
 
 }
