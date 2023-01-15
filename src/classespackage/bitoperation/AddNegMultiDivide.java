44f17ccc5d873e1460759f9f2fa625cd7c9e4b3b
ShuoWen5656 <2260755767@qq.com>
2023-01-15 13:41:17 +0800

    [新增代码][二轮测试并发表]1、不用算数运算计算整数的加减乘除

--- src/classespackage/bitoperation/AddNegMultiDivide.java
+++ src/classespackage/bitoperation/AddNegMultiDivide.java
@@ -24,7 +24,7 @@ public class AddNegMultiDivide {
             while (carry != 0){
                 int tem = res;
                 res = res ^ carry;
-                carry = (tem&res) << 1;
+                carry = (tem&carry) << 1;
             }
             return res;
         }catch (Exception e){
@@ -140,8 +140,134 @@ public class AddNegMultiDivide {
      * @return
      */
     public static int negNum(int a){
-        return add(~a, 1);
+        return addCp1(~a, 1);
     }
+
+
+    /**
+     * a+b
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int addCp1(int a, int b) {
+        int sum = a;
+        while (b != 0) {
+            // 无进位结果
+            int x = sum ^ b;
+            // 进位
+            b = (sum & b) << 1;
+            sum = x;
+        }
+        return sum;
+    }
+
+    /**
+     * a-b
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int subCp1(int a, int b) {
+        // a+ b的补码即可,取反+1，符号位一起计算
+        return addCp1(a, negNum(b));
+    }
+
+    /**
+     * 获取当前值的补码
+     * @param a
+     * @return
+     */
+    public static int negNumCp1(int a) {
+        return addCp1(~a, 1);
+    }
+
+    /**
+     * a * b
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int multiCp1(int a, int b) {
+        int res = 0;
+        while (b != 0) {
+            if ((b & 1) != 0) {
+                res = addCp1(res, a);
+            }
+            b >>>= 1;
+            a <<= 1;
+        }
+        return res;
+    }
+
+
+    /**
+     * 除法最通常的情况
+     * 但是有个缺陷就是 补码的负数比正数多一个最小值，最小值无法转正数所以当最小值计算时需要另外判断
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int divCp1(int a, int b) {
+        // 这里去一下符号
+        int x = a < 0 ? negNumCp1(a) : a;
+        int y = b < 0 ? negNumCp1(b) : b;
+        // 商
+        int res = 0;
+        // 因为不能存在算数计算，所以这里--i也用的sub函数减法
+        for (int i = 31; i >= 0; i = subCp1(i, 1)) {
+            // 先尝试除数b左移位i位是否小于等于被除数a
+            if ((x >> i) >= b) {
+                // 说明商1
+                res |= 1 << i;
+                // 减
+                x = subCp1(x, y << i);
+            }
+        }
+        return res;
+    }
+
+
+    /**
+     * 考虑了全部情况的除法
+     * @return
+     */
+    public static int divideCp1(int a, int b) {
+        if (b == 0) {
+            throw new RuntimeException("除数为0");
+        }
+        if (a == Integer.MIN_VALUE && b == Integer.MIN_VALUE) {
+            return 1;
+        }else if (a != Integer.MIN_VALUE && b == Integer.MIN_VALUE) {
+            return 0;
+        }else if (a == Integer.MIN_VALUE && b == Integer.MIN_VALUE) {
+            // 首先计算a+1/b
+            int res1 = divCp1(a+1, b);
+            // 再乘回去求和原来值的差
+            int sub = a - multiCp1(res1, b);
+            // 返回结果,如果偏差值刚好满一个b，那么说明res1 + 1
+            return res1 + sub/b;
+        }else {
+            // 最正常的情况
+            return divCp1(a, b);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(add(4, 2));
+    }
+
+
 
 
     @Override
