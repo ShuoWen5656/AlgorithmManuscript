3c9802296bbfc7915da629ca2e27950527731060
ShuoWen5656 <2260755767@qq.com>
2023-02-03 20:22:57 +0800

    [新增代码][二轮测试并发表]1、找到只出现一次的数

--- src/classespackage/bitoperation/FindOnePresent.java
+++ src/classespackage/bitoperation/FindOnePresent.java
@@ -76,4 +76,76 @@ public class FindOnePresent {
         return res;
     }
 
+
+    /**
+     * 求arr中只出现一次的数
+     * @param arr
+     * @return
+     */
+    public static int getOneNumCp1(int[] arr, int k) {
+        // 假设该数为32位的k进制数,每一位都不大于k
+        int[] kE0 = new int[32];
+        for (int i = 0; i < arr.length; i++) {
+            // 先将arr[i]转k进制
+            int[] kE1 = convert2k(arr[i], k);
+            // 无进位相加
+            kE0 = sumK(kE0, kE1, k);
+        }
+        // 转成10进制数
+        return convert2Tem(kE0, k);
+    }
+
+
+    /**
+     * 将10进制转k进制并返回
+     * @param num
+     * @return
+     */
+    private static int[] convert2k(int num, int k) {
+        int n = num;
+        int index = 0;
+        int[] res = new int[32];
+        while (n != 0) {
+            res[index++] = n % k;
+            n = n/k;
+        }
+        return res;
+    }
+
+    /**
+     * k进制相加
+     * @param kE0
+     * @param kE1
+     * @param k
+     * @return
+     */
+    private static int[] sumK(int[] kE0, int[] kE1, int k) {
+        int[] res = new int[32];
+        for (int i = 0; i < kE0.length; i++) {
+            res[i] = (kE0[i] + kE1[i]) % k;
+        }
+        return res;
+    }
+
+
+    /**
+     * 将k进制转10进制
+     * @param kE0
+     * @param k
+     * @return
+     */
+    private static int convert2Tem(int[] kE0, int k) {
+        int res = 0;
+        for (int i = 0; i < kE0.length; i++) {
+            res += Math.pow(k, i) * kE0[i];
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(getOneNumCp1(new int[]{5,5,5,6,4,4,4}, 3));
+    }
+
+
 }
