6765202ca8766bef08808457f1f5341dd80ed4d4
ShuoWen5656 <2260755767@qq.com>
2023-02-05 11:29:12 +0800

    [新增代码][二轮测试并发表]1、转圈打印矩阵

--- src/classespackage/arrayAndMartrix/SpiralOrderPrint.java
+++ src/classespackage/arrayAndMartrix/SpiralOrderPrint.java
@@ -71,4 +71,81 @@ public class SpiralOrderPrint {
     }
 
 
+    /**
+     * 二轮测试：转圈打印
+     */
+    public static void spiralOrderPrintCp1(int[][] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        // 横向
+        int len = arr.length;
+        // 纵向
+        int len2 = arr[0].length;
+        // 打印边界
+        int boundR = len/2 + 1;
+        int boundL = len2/2 + 1;
+        int i = 0;
+        int j = 0;
+        while (i < boundR && j < boundL) {
+            processCp(arr , i, j, len2, len);
+            len-=2;
+            len2-=2;
+            i++;
+            j++;
+        }
+    }
+
+    /**
+     * 二轮测试：打印一圈，支持长方形
+     * @param arr
+     * @param i 起点横坐标
+     * @param j 起点纵坐标
+     * @param wide 宽度
+     * @param height 高度
+     */
+    private static void processCp(int[][] arr, int i, int j, int wide, int height) {
+        int i1 = i;
+        int j1 = j;
+        int wideBound = j + wide;
+        int heightBound = i + height;
+        // 横向
+        while (j1 < wideBound) {
+            System.out.println(arr[i1][j1]);
+            j1++;
+        }
+        // j1此时超出去一个
+        j1--;
+        i1++;
+        // 纵向
+        while (i1 < heightBound) {
+            System.out.println(arr[i1][j1]);
+            i1++;
+        }
+        i1--;
+        j1--;
+        // 横向回
+        while (j1 >= j) {
+            System.out.println(arr[i1][j1]);
+            j1--;
+        }
+        j1++;
+        i1--;
+        // 纵向回
+        while (i1 > i) {
+            System.out.println(arr[i1][j1]);
+            i1--;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        spiralOrderPrintCp1(new int[][]{
+                {1,2,3},
+                {5,6,7},
+                {9,10,11},
+                {13,14,15}
+        });
+    }
+
 }
