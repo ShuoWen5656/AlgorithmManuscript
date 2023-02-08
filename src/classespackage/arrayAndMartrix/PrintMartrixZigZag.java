9c1d658a9b8585743c87a0c2f32c4f356df52a66
ShuoWen5656 <2260755767@qq.com>
2023-02-08 20:17:30 +0800

    [新增代码][二轮测试并发表]1、之字形打印矩阵

--- src/classespackage/arrayAndMartrix/PrintMartrixZigZag.java
+++ src/classespackage/arrayAndMartrix/PrintMartrixZigZag.java
@@ -52,4 +52,82 @@ public class PrintMartrixZigZag {
         }
     }
 
+
+    /**
+     * 二轮测试：之字形打印矩阵
+     * @param arr
+     */
+    public static void printMartrixZCp1(int[][] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        int len1 = arr.length;
+        int len2 = arr[0].length;
+        // 循环次数
+        int count = len1 + len2 - 1;
+        int index = 0;
+        // 方向：true为向上
+        boolean forward = true;
+        // 初始化起点,start[0]纵坐标i,start[1]横坐标j
+        int[] start = new int[]{0,0};
+        while (index < count) {
+            process(arr, start, forward);
+            // 计算下一个起点
+            if (forward) {
+                if (start[1] + 1 >= len2) {
+                    start[0] = start[0]+1;
+                }else if (start[0] - 1 < 0) {
+                    start[1] = start[1] + 1;
+                }
+            }else {
+                // 向下
+                if (start[0] + 1 >= len1) {
+                    start[1] = start[1]+1;
+                }else if (start[1] - 1 < 0) {
+                    start[0] = start[0] + 1;
+                }
+            }
+            // 转换方向
+            forward = !forward;
+            index ++;
+        }
+    }
+
+    /**
+     * 从arr的ij开始，按照forward方向打印到边界
+     * @param arr
+     * @param forward
+     */
+    private static void process(int[][] arr, int[] start, boolean forward) {
+        int len1 = arr.length;
+        int len2 = arr[0].length;
+        while (start[0] < len1 && start[0] >=0 && start[1] >= 0 && start[1] < len2) {
+            System.out.print(arr[start[0]][start[1]] + "   ");
+            if (forward) {
+                if (start[0] - 1 < 0 || start[1] + 1 >= len2) {
+                    // 越界退出
+                    break;
+                }
+                start[0] = start[0] - 1;
+                start[1] = start[1] + 1;
+            }else {
+                if (start[0] + 1 >= len1 || start[1] - 1 < 0) {
+                    // 越界退出
+                    break;
+                }
+                start[0] = start[0] + 1;
+                start[1] = start[1] - 1;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        printMartrixZCp1(new int[][] {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12}
+        });
+    }
+
 }
