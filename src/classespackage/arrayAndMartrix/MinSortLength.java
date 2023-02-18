f3971ab36568d8bdd09f4ce98dc972760e5fc0ed
ShuoWen5656 <2260755767@qq.com>
2023-02-18 14:00:38 +0800

    [新增代码][二轮测试并发表]1、数组的最小排序子数组数组长度

--- src/classespackage/arrayAndMartrix/MinSortLength.java
+++ src/classespackage/arrayAndMartrix/MinSortLength.java
@@ -47,4 +47,56 @@ public class MinSortLength {
             return 0;
         }
     }
+
+
+    /**
+     * 二轮测试：需要排序的最短子数组长度
+     * @param arr
+     * @return
+     */
+    public static int sortLenCp1(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        // 边界值，为遍历到目前，最值数
+        int bound = 0;
+        // 遍历到目前，在最值数另一边导致顺序乱序的最左边或者最右边的数
+        int indexleft = -1, rightIndex = -1;
+        int i = arr.length-1;
+        bound = arr[arr.length-1];
+        while (i >= 0) {
+            if (arr[i] < bound) {
+                // 最值更新
+                bound = arr[i];
+            }else if (arr[i] > bound){
+                // 更新需要移动到i右边的最左边的数
+                indexleft = i;
+            }
+            i--;
+        }
+        // 从左到右遍历
+        i = 0;
+        bound = arr[0];
+        while (i < arr.length) {
+            if (arr[i] > bound) {
+                bound = arr[i];
+            }else if (arr[i] < bound){
+                rightIndex = i;
+            }
+            i++;
+        }
+        if (indexleft == -1 && rightIndex == -1){
+//            整个数组都是有序的
+            return 0;
+        }else{
+            return rightIndex - indexleft + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sortLenCp1(new int[]{1,2,3,2,35,8,9}));
+    }
+
+
+
 }
