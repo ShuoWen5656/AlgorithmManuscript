0884921f5b5d5fac5783ed9d813a12f67bbf9d6d
ShuoWen5656 <2260755767@qq.com>
2023-02-26 13:00:19 +0800

    [新增代码][二轮测试并发表]1、在排好序的矩阵中找数

--- src/classespackage/arrayAndMartrix/IsContainsInSortMatrix.java
+++ src/classespackage/arrayAndMartrix/IsContainsInSortMatrix.java
@@ -18,7 +18,7 @@ public class IsContainsInSortMatrix {
     public static boolean isContains(int[][] array, int k){
         try {
             int row = 0;
-            int col = array.length - 1;
+            int col = array[0].length - 1;
             while (row < array.length && col > -1){
                 if(k == array[row][col]){
                     return true;
@@ -37,6 +37,43 @@ public class IsContainsInSortMatrix {
         }
     }
 
+
+    /**
+     * 二轮测试：在排好序的矩阵中找数
+     * @param arr
+     * @param k
+     * @return
+     */
+    public static boolean isContainsCp1(int[][] arr, int k) {
+        if (arr == null || arr.length == 0) {
+            return false;
+        }
+        int row = arr.length - 1;
+        int col = 0;
+        while (row >= 0 && col <= arr[0].length - 1) {
+            int cur = arr[row][col];
+            if (cur == k) {
+                return true;
+            }else if (cur > k) {
+                // 当前行不会存在k
+                row--;
+            }else {
+                // 当前列不会存在k
+                col++;
+            }
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isContainsCp1(new int[][]{
+                {0,1,2,5},
+                {2,3,4,7},
+                {4,4,4,8},
+                {5,7,7,9}
+        }, 7));
+    }
 
 
 
