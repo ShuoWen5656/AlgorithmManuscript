8f241234883fb31536cd33f1de586fdacd03fa53
ShuoWen5656 <2260755767@qq.com>
2023-03-08 22:04:08 +0800

    [新增代码][二轮测试并发表]1、最大累乘积的子数组

--- src/classespackage/arrayAndMartrix/GetMaxMulti.java
+++ src/classespackage/arrayAndMartrix/GetMaxMulti.java
@@ -1,5 +1,7 @@
 package classespackage.arrayAndMartrix;
 
+import java.util.Collections;
+
 /**
  * @author swzhao
  * @data 2022/6/20 19:56
@@ -41,5 +43,31 @@ public class GetMaxMulti {
     }
 
 
+    /**
+     * 二轮测试：获取最大的累乘数组
+     * @param arr
+     * @return
+     */
+    public static double getMaxMulti(double[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new RuntimeException("invalid arr");
+        }
+        double max = arr[0];
+        double min = arr[0];
+        double res = arr[0];
+        for (int i = 1; i < arr.length; i ++) {
+            double curMax = arr[i] * max;
+            double curMin = arr[i] * min;
+            max = Math.max(arr[i], Math.max(curMax, curMin));
+            min = Math.min(arr[i], Math.min(curMax, curMin));
+            res = Math.max(res, max);
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(getMaxMulti(new double[]{0.1, 0.1, 100}));
+    }
 
 }
