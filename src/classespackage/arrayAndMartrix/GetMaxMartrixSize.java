0a528849c9b2a223904cfe1d719a459d362a8f23
ShuoWen5656 <2260755767@qq.com>
2023-03-11 10:37:40 +0800

    [新增代码][二轮测试并发表]1、边长为1的最大矩阵边长

--- src/classespackage/arrayAndMartrix/GetMaxMartrixSize.java
+++ src/classespackage/arrayAndMartrix/GetMaxMartrixSize.java
@@ -111,6 +111,82 @@ public class GetMaxMartrixSize {
     }
 
 
+    /**
+     * 二轮测试：获取边长为1的最大矩阵边长
+     * @param arr
+     * @return
+     */
+    public static int getMaxMatrixCp1(int[][] arr) {
+        if (arr == null || arr.length == 0 || arr[0].length == 0) {
+            return -1;
+        }
+        // 记录每一个坐标开始往右和往下最多能走多少个1
+        int[][] right = new int[arr.length][arr[0].length];
+        int[][] down = new int[arr.length][arr[0].length];
+        // 填充
+        fillMapCp1(arr, right, down);
+        // 计算每一种矩阵边长是否能够满足，从最大的算起
+        for (int size = Math.min(arr.length, arr[0].length); size > 0; size--) {
+            // 尝试矩阵是否能够存在size大小的1矩阵
+            if (hasSizeCp1(right, down, size)) {
+                return size;
+            }
+        }
+        return 0;
+    }
+
+
+    private static void fillMapCp1(int[][] arr, int[][] right, int[][] down) {
+        // 先填充右列
+        int col = arr[0].length-1;
+        int row = arr.length-1;
+        right[row][col] = arr[row][col] == 0 ? 0 : 1;
+        down[row][col] = arr[row][col] == 0 ? 0 : 1;
+        for (int i = row-1; i >= 0; i--) {
+            right[i][col] = arr[i][col];
+            down[i][col] = arr[i][col] == 0 ? 0 : down[i+1][col] + 1;
+        }
+        // 填充下行
+        for (int j = col-1; j >= 0; j--) {
+            down[row][j] = arr[row][j];
+            right[row][j] = arr[row][j] == 0 ? 0 : right[row][j+1] + 1;
+        }
+        // 填充所有
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col-1; j >= 0; j--) {
+                right[i][j] = arr[i][j] == 0 ? 0 : right[i][j+1] + 1;
+                down[i][j] = arr[i][j] == 0 ? 0 : down[i+1][j] + 1;
+            }
+        }
+    }
+
+
+
+    private static boolean hasSizeCp1(int[][] right, int[][] down, int size) {
+        for (int i = 0; i < down.length - size + 1; i++) {
+            for (int j = 0; j < right[0].length - size + 1; j++) {
+                // 判断当前点是否存在size大小的矩阵,四个顶点
+                if (right[i][j] < size || down[i][j] < size
+                        || down[i][j + size - 1] < size
+                        || right[i + size - 1][j] < size) {
+                    // 存在边长小于size，说明当前点不能组成
+                    continue;
+                }
+                // 说明存在
+                return true;
+            }
+        }
+        // 说明不存在
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMaxMatrixCp1(new int[][]{
+                {0, 1,1,1},
+                {0, 1,0,1},
+                {0, 1,1,1}
+        }));
+    }
 
 
 }
