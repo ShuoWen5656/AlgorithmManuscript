9adf0bbfbd252017da362ce31cb56857cd50d018
ShuoWen5656 <2260755767@qq.com>
2023-03-28 21:38:47 +0800

    [新增代码][二轮测试并发表]1、判断点是否在三角形中

--- src/classespackage/other/TriangleInside.java
+++ src/classespackage/other/TriangleInside.java
@@ -125,5 +125,132 @@ public class TriangleInside {
     }
 
 
+    /**
+     * 二轮测试：判断点是否在三角形的内部
+     * 方法一：面积法
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     * @param x3
+     * @param y3
+     * @param x
+     * @param y
+     * @return
+     */
+    public static boolean isInsideCp1(double x1, double y1,
+                                      double x2, double y2,
+                                      double x3, double y3,
+                                      double x, double y) {
+        return getSquare(x1, y1, x2, y2, x, y) + getSquare(x1, y1, x3, y3, x, y) + getSquare(x3, y3, x2, y2, x, y)
+                <= getSquare(x1, y1, x2, y2, x3, y3);
+    }
+
+    /**
+     * 给定三个点，求三角形面积
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     * @param x3
+     * @param y3
+     * @return
+     */
+    private static double getSquare(double x1, double y1,
+                                    double x2, double y2,
+                                    double x3, double y3) {
+        // 先求三边
+        double len1 = getLen(x1, y1, x2, y2);
+        double len2 = getLen(x1, y1, x3, y3);
+        double len3 = getLen(x2, y2, x3, y3);
+        double p = (len1 + len2 + len3)/2;
+        return Math.sqrt(p * (p-len1) * (p - len2) * (p - len3));
+    }
+
+    /**
+     * 计算两点之间的距离
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     * @return
+     */
+    private static double getLen(double x1, double y1,
+                                 double x2, double y2) {
+        return Math.sqrt((x1-x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
+
+
+    /**
+     * 二轮测试：判断点是否在三角形内
+     * 方法二：向量法
+     * 该方法不存在精度问题导致的结果不准确
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     * @param x3
+     * @param y3
+     * @param x
+     * @param y
+     * @return
+     */
+    public static boolean isInsideCp2(double x1, double y1,
+                                      double x2, double y2,
+                                      double x3, double y3,
+                                      double x, double y) {
+        // 判断点是否是顺时针顺序，如果不是则调整一下
+        if (getCrossCp2(x1, y1, x3, y3, x2, y2) < 0) {
+            // 说明x2，y2在右边,交换3和2
+            double temx = x2;
+            double temy = y2;
+            x2 = x3;
+            y2 = y3;
+            x3 = temx;
+            y3 = temy;
+        }
+        // 判断叉乘是否都是小于0
+        if (getCrossCp2(x1, y1, x2, y2, x, y) <= 0
+                && getCrossCp2(x2, y2, x3, y3, x, y) <= 0
+                && getCrossCp2(x3, y3, x1, y1, x, y) <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 计算x，y点在(x1, y2) -> (x2, y2) 向量的左边还是右边
+     * 大于0，左边 小于0，右边
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     * @param x
+     * @param y
+     * @return
+     */
+    public static double getCrossCp2(double x1, double y1,
+                                     double x2, double y2,
+                                     double x, double y) {
+        // 以x1，y1为基准
+        double xs = x2 - x1;
+        double ys = y2 - y1;
+        double xs1 = x - x1;
+        double ys1 = y - y1;
+        // 计算叉乘
+        return xs * ys1 - ys * xs1;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println(isInsideCp2(0 ,0, 2, 0, 1, 2, 1, 1));
+    }
+
+
+
 
 }
