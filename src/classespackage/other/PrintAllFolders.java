63182b8d5b172552cca1d845a5072c0ae718c216
ShuoWen5656 <2260755767@qq.com>
2023-03-29 20:15:09 +0800

    [新增代码][二轮测试并发表]1、折纸问题

--- src/classespackage/other/PrintAllFolders.java
+++ src/classespackage/other/PrintAllFolders.java
@@ -1,5 +1,7 @@
 package classespackage.other;
 
+import classespackage.stackAndQueue.catDogQueue.Pet;
+
 /**
  * @author swzhao
  * @data 2022/7/4 21:40
@@ -34,5 +36,59 @@ public class PrintAllFolders {
         // 打印左边
         process(i+1, num, false);
     }
+
+
+    /**
+     * 二轮测试：纸张折叠num次之后，打印折痕
+     * @param num
+     */
+    private static void printAllFoldersCp1(int num) {
+        if (num <= 0) {
+            return;
+        }
+        processCp1(num, null, null);
+    }
+
+    /**
+     * 中序遍历，递归打印折痕
+     * @param num
+     * @param parent 父节点是上还是下
+     * @param isLeft 是否左子节点
+     */
+    private static void processCp1(int num, Boolean parent, Boolean isLeft) {
+        if (num == 0) {
+            // 已经减到0了直接返回
+            return;
+        }
+        Boolean cur = false;
+        // 先判断自己的是上还是下
+        if (parent == null || isLeft == null) {
+            // 根节点直接下
+            cur = false;
+        }else if (!parent && isLeft) {
+            // 根节点是下并且自己是左子树
+            cur = false;
+        }else if (!parent && !isLeft) {
+            // 根节点是下，并且自己是右子树
+            cur = true;
+        }else if (parent && isLeft) {
+            // 上左子树
+            cur = false;
+        }else if (parent && !isLeft) {
+            cur = true;
+        }
+        // 判断完成后开始下一轮
+        processCp1(num - 1, cur, true);
+        // 输出自己
+        System.out.println(cur ? "上" : "下");
+        // 输出右边
+        processCp1(num - 1, cur, false);
+    }
+
+
+    public static void main(String[] args) {
+        printAllFolders(4);
+    }
+
 
 }
