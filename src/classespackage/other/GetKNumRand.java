ccfa520f0407280b7af0a98c6795403c7939ee9d
ShuoWen5656 <2260755767@qq.com>
2023-04-01 20:52:00 +0800

    [新增代码][二轮测试并发表]1、蓄水池算法

--- src/classespackage/other/GetKNumRand.java
+++ src/classespackage/other/GetKNumRand.java
@@ -1,5 +1,7 @@
 package classespackage.other;
 
+import classespackage.CommonUtils;
+
 /**
  * @author swzhao
  * @data 2022/7/5 21:22
@@ -43,6 +45,42 @@ public class GetKNumRand {
         return res;
     }
 
+
+
+    /**
+     * 二轮测试：蓄水池算法
+     * @param k
+     * @param max
+     * @return
+     */
+    public static int[] getKNumRandCp1(int k, int max) {
+        int[] container = new int[k];
+        // 初始化
+        //for (int i = 0; i < max; i++) {
+        //    container[i] = i + 1;
+        //}
+        for (int i = 0; i < max; i++) {
+            if (i < k) {
+                container[i] = i + 1;
+                continue;
+            }
+            // 如果随机数没有超过k，则随机替换一个
+            if (rand(i) <= k) {
+                container[randCp1(k) - 1] = i;
+            }
+        }
+        return container;
+    }
+
+
+    public static int randCp1(int num) {
+        return (int)(Math.random() * num + 1);
+    }
+
+
+    public static void main(String[] args) {
+        CommonUtils.printArr(getKNumRandCp1(5, 9));
+    }
 
 
 }
