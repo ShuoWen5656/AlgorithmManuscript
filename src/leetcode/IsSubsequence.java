52480fb5553471f1f26eb79e2a7b3a6990a95651
ShuoWen5656 <2260755767@qq.com>
2023-12-30 10:51:01 +0800

    [新增代码][leetcode算法练习]1、判断子序列进阶解法

--- src/leetcode/IsSubsequence.java
+++ src/leetcode/IsSubsequence.java
@@ -9,7 +9,7 @@ public class IsSubsequence {
 
 
     public static void main(String[] args) {
-        System.out.println(solution("abc", "ahbgdc"));
+        System.out.println(solution2("abc", "ahbgdc"));
     }
 
     public static boolean solution(String s, String t) {
@@ -38,5 +38,46 @@ public class IsSubsequence {
         }
         return true;
     }
+
+    /**
+     * 进阶解法，动态规划，能够判断s的任何一个子串是否在t中
+     * @param s
+     * @param t
+     * @return
+     */
+    public static boolean solution2(String s, String t) {
+        char[] charS = s.toCharArray();
+        int sLen = s.length();
+        char[] chatT = t.toCharArray();
+        int tLen = t.length();
+
+        // dp[i][j] 代表 从t[i]开始出发，字母j第一次出现的地方
+        int[][] dp = new int[t.length() + 1][26];
+        // 初始化dp,从tlen开始出发的字母第一次出现的地方都应该是tlen（不存在的意思）
+        for (int i = 0; i < 26; i++) {
+            dp[tLen][i] = tLen;
+        }
+        // 计算dp
+        for (int i = dp.length-2; i >= 0; i--) {
+            for (int j = 0; j < 26; j++) {
+                // 如果当前char是j，则直接为i位置，否则继承前一个位置
+                dp[i][j] = chatT[i] - 'a' == j ? i : dp[i+1][j];
+            }
+        }
+        int index = 0;
+        // 计算答案
+        for (int i = 0; i < sLen; i++) {
+            // 从index开始判断char第一次出现的地方
+            if (dp[index][charS[i] - 'a'] == tLen) {
+                // 说明t中从index开始不存在这个字符
+                return false;
+            }else {
+                // 跳到这个字符第一次出现的地方，然后判断下一个字符
+                index = dp[index][charS[i] - 'a'] + 1;
+            }
+        }
+        return true;
+    }
+
 
 }
